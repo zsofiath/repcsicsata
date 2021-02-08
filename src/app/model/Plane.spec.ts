f802e3e591880b60cdc1bfb0b86879f3e2a05364
zsofi <zsofiath@gmail.com>
2021-02-08 09:39:51 +0100

    left facing plane

--- src/app/model/Plane.spec.ts
+++ src/app/model/Plane.spec.ts
@@ -1,6 +1,7 @@
 import { DirectionEnum } from "../constants/DirectionEnum";
 import Coordinate from "./Coordinate";
 import Plane from "./Plane";
+import PlaneDrawerLeft from "./planeDrawer/PlaneDrawerLeft";
 import PlaneDrawerRight from "./planeDrawer/PlaneDrawerRight";
 import PlaneDrawerUp from "./planeDrawer/PlaneDrawerUp";
 
@@ -84,6 +85,43 @@ describe('Plane', () => {
         expect(planeCoordinates[9].y).toEqual(4);
     });
 
-    
+    it('should return its coordinates, direction:left', () => {
+        
+        let centerPosition = new Coordinate();
+        centerPosition.x = 2;
+        centerPosition.y = 3;
+
+        let drawer = new Plane(new PlaneDrawerLeft(), centerPosition);
+         
+        let planeCoordinates = drawer.getCoordinates();
+
+        // head
+        expect(planeCoordinates[0].x).toEqual(1);
+        expect(planeCoordinates[0].y).toEqual(3);
+
+        // wings
+        expect(planeCoordinates[1].x).toEqual(2);
+        expect(planeCoordinates[1].y).toEqual(1);
+        expect(planeCoordinates[2].x).toEqual(2);
+        expect(planeCoordinates[2].y).toEqual(2);
+        expect(planeCoordinates[3].x).toEqual(2);
+        expect(planeCoordinates[3].y).toEqual(3);
+        expect(planeCoordinates[4].x).toEqual(2);
+        expect(planeCoordinates[4].y).toEqual(4);
+        expect(planeCoordinates[5].x).toEqual(2);
+        expect(planeCoordinates[5].y).toEqual(5);
+
+        // body
+        expect(planeCoordinates[6].x).toEqual(3);
+        expect(planeCoordinates[6].y).toEqual(3);
+
+        //tail
+        expect(planeCoordinates[7].x).toEqual(4);
+        expect(planeCoordinates[7].y).toEqual(2);
+        expect(planeCoordinates[8].x).toEqual(4);
+        expect(planeCoordinates[8].y).toEqual(3);
+        expect(planeCoordinates[9].x).toEqual(4);
+        expect(planeCoordinates[9].y).toEqual(4);
+    });
     
 }); 
---
+++ src/app/model/planeDrawer/PlaneDrawerLeft.ts
@@ -0,0 +1,45 @@
+import BoardCell from "../BoardCell";
+import Coordinate from "../Coordinate";
+import IPlaneDrawer from "./IPlaneDrawer";
+
+export default class PlaneDrawerLeft implements IPlaneDrawer {
+
+    constructor(){
+    }
+    drawHead(planeCenter: Coordinate): Coordinate[] {
+        let c = new Coordinate();
+        c.x = planeCenter.x-1;
+        c.y = planeCenter.y;
+        return [c];
+    }
+    drawWings(planeCenter: Coordinate): Coordinate[] {
+        let wings = [];
+
+        for (let i = 0; i < 5; i++) {
+            let c = new Coordinate();
+            c.x = planeCenter.x;
+            c.y = planeCenter.y+i-2;
+            wings.push(c);
+        }
+        return wings;
+    }
+    drawBody(planeCenter: Coordinate): Coordinate[] {
+        let b = new Coordinate();
+        b.x = planeCenter.x+1;
+        b.y = planeCenter.y;
+        return [b];
+    }
+    drawTail(planeCenter: Coordinate): Coordinate[] {
+        let tail = [];
+
+        for (let i = 0; i < 3; i++) {
+            let c = new Coordinate();
+            c.x = planeCenter.x+2;
+            c.y = planeCenter.y+i-1;
+            tail.push(c)  
+        }
+
+        return tail;
+    }
+
+}
