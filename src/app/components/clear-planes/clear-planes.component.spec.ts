3b1bf23bb7cf565279a7b1e9a88fc5b769f1ef0b
zsofi <zsofiath@gmail.com>
2021-02-10 19:29:39 +0100

    clear planes when clear button is clicked

--- src/app/components/clear-planes/clear-planes.component.spec.ts
+++ src/app/components/clear-planes/clear-planes.component.spec.ts
@@ -1,5 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, flush, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { BoardCellStateEnum } from 'src/app/constants/BoardCellStatesEnum';
+import BoardCell from 'src/app/model/BoardCell';
 import Plane from 'src/app/model/Plane';
 
 import { ClearPlanesComponent } from './clear-planes.component';
@@ -28,5 +30,33 @@ describe('ClearPlanesComponent', () => {
   it('should have a button with class .clear-all', () => {
     expect(fixture.debugElement.queryAll(By.css('.clear-all')).length).toBe(1);
   });
+
+  fit('should delete planes, but not changing reference', fakeAsync(() => {
+    let arr = [new Plane(null, null), new Plane(null, null), new Plane(null, null)];
+    component.planes = arr;
+    component.cells = [[]];
+
+    let el = fixture.debugElement.queryAll(By.css('.clear-all'))[0];
+
+    el.triggerEventHandler('click', null);
+
+    flush();
+
+    expect(arr.length).toEqual(0);
+    expect(Object.is(arr, component.planes)).toBeTruthy();
+  }));
+
+  it('should set all cells free', fakeAsync(() => {
+    component.cells = [[new BoardCell(0,0)]];
+    component.planes = [];
+
+    let el = fixture.debugElement.queryAll(By.css('.clear-all'))[0];
+
+    el.triggerEventHandler('click', null);
+
+    flush();
+    
+    expect(component.cells[0][0].state).toEqual(BoardCellStateEnum.FREE);
+  }));
 
 });
--- src/app/components/clear-planes/clear-planes.component.ts
+++ src/app/components/clear-planes/clear-planes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BoardCellStateEnum } from 'src/app/constants/BoardCellStatesEnum';
+import BoardCell from 'src/app/model/BoardCell';
+import Plane from 'src/app/model/Plane';
 
 @Component({
   selector: 'app-clear-planes',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClearPlanesComponent implements OnInit {
 
+  @Input() planes: Plane[];
+  @Input() cells: BoardCell[][];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clearPlanes(){
+    
+    let numberOfPlanes = this.planes.length;
+    for (let i = 0; i < numberOfPlanes; i++) {
+      this.planes.pop(); 
+    }
+
+    this.cells.forEach(row => {
+      row.forEach(cell=>{
+        cell.state = BoardCellStateEnum.FREE;
+      })
+    });
   }
 
 }
