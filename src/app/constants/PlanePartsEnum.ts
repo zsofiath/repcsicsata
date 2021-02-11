d04b3a467dd11d8109431ce09f5ef5783ed739d8
zsofi <zsofiath@gmail.com>
2021-02-11 15:12:06 +0100

    plane parts enum is created

---
+++ src/app/constants/PlanePartsEnum.ts
@@ -0,0 +1,16 @@
+export enum PlaneParts {
+    HEAD = "head",
+
+    WING1 = "wing1",
+    WING2 = "wing2",
+    WING3 = "wing3",
+    WING4 = "wing4",
+    WING5 = "wing5",
+
+    BODY = "body",
+
+    TAIL1 = "tail1",
+    TAIL2 = "tail2",
+    TAIL3 = "tail3",
+
+}
