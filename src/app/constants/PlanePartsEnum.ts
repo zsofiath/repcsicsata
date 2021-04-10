e7f88061ad9d43193fcc623b8e688ba6c4429482
zsofi <zsofiath@gmail.com>
2021-04-10 11:06:37 +0200

    refactor plane part revealing

--- src/app/constants/PlanePartsEnum.ts
+++ src/app/constants/PlanePartsEnum.ts
@@ -1,19 +1,19 @@
 export enum PlanePartsEnum {
     HEAD = "head",
 
-    WING1 = "wing1",
-    WING2 = "wing2",
-    WING3 = "wing3",
-    WING4 = "wing4",
-    WING5 = "wing5",
+    WING1 = "wing-1",
+    WING2 = "wing-2",
+    WING3 = "wing-3",
+    WING4 = "wing-4",
+    WING5 = "wing-5",
 
     BODY = "body",
 
-    TAIL1 = "tail1",
-    TAIL2 = "tail2",
-    TAIL3 = "tail3",
+    TAIL1 = "tail-1",
+    TAIL2 = "tail-2",
+    TAIL3 = "tail-3",
 
-    TARGET_CROSS ='targetcross',
+    TARGET_CROSS ='target-cross',
     HIT = 'hit'
 
 }
